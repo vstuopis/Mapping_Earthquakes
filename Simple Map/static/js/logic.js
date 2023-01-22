f2e32eeda017bd0daa47637528e672e7e45b9e41
vstuopis <113248280+vstuopis@users.noreply.github.com>
2023-01-22 15:33:25 -0500

    City with radius population insert.

--- Simple Map/static/js/logic.js
+++ Simple Map/static/js/logic.js
@@ -4,16 +4,26 @@ console.log("working");
 // Create the map object with a center and zoom level.
 let map = L.map('mapid').setView([40.7, -94.5], 4);
 
-/// Create the map object with a center and zoom level. - Second way to do it
-///let map = L.map("mapid", {
-///    center: [
-///      40.7, -94.5
-///    ],
-///    zoom: 4
-///  });
+// Get data from cities.js
+let cityData = cities;
 
-// We create the tile layer that will be the background of our map.
-let streets = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+  /// Loop through the cities array and create one marker for each city. - This just logs the values in the console
+///cities.forEach(function(city) {
+///    console.log(city)
+///   });
+
+   // Loop through the cities array and create one marker for each city.
+cityData.forEach(function(city) {
+    console.log(city)
+    L.circleMarker(city.location, {
+      radius: city.population/100000
+    })
+    .bindPopup("<h2>" + city.city + ", " + city.state + "</h2> <hr> <h3>Population " + city.population.toLocaleString() + "</h3>")
+  .addTo(map);
+});
+
+// We create the tile layer that will be the background of our map with a dark tile layout.
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
     id: 'mapbox/streets-v11',
